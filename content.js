a85b0f87f7570a1b16770960cf65cbe9c7086a8b
IvnoGood <afonsovic2009@gmail.com>
2025-01-28 18:14:10 +0100

    pull

--- content.js
+++ content.js
@@ -1,34 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBA-p71gjjXpSEZdOezXX-fxGP-jWqtekE",
-  authDomain: "workmissingpronote.firebaseapp.com",
-  databaseURL:
-    "https://workmissingpronote-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "workmissingpronote",
-  storageBucket: "workmissingpronote.firebasestorage.app",
-  messagingSenderId: "377010182640",
-  appId: "1:377010182640:web:5080d000472adaa79fe535",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-const starCountRef = ref(database);
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data);
-});
-
 const observer = new MutationObserver((mutations) => {
   const y = document.getElementsByClassName("liste-imbriquee")[0];
   if (y) {
+    const fetchDb = async (reference) => {
+      try {
+        const response = await fetch("http://localhost:5000/get-db", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ reference }),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.error || "Failed to fetch token");
+        }
+        return data.token;
+      } catch (err) {
+        console.error("Error fetching token:", err);
+        throw err;
+      }
+    };
+
+    console.log(fetchDb("/"));
+
     const matiere = "SVT";
     const date = "06/01";
     const contenu = "Casser les burnes";
